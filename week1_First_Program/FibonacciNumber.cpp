4451bca3fe80f90c6dd1fcb4a4d261131df949a8
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-08-13 20:44:26 +0700

    change from another branch

--- week1_First_Program/FibonacciNumber.cpp
+++ week1_First_Program/FibonacciNumber.cpp
@@ -1,17 +1,24 @@
 #include<stdio.h>
 int main()
 {
-	int n,t1,t2,t3,count=0 ;
+	int n, t1, t2, t3 = 0, count = 0, keep = 0, i;
 	t1 = 0;
 	t2 = 1;
-	scanf_s("%d",&n);
-	while (count < n)
+	printf("Program sum of all even term in fibonacci sequence");
+	printf("\n\nEnter number of term : ");
+	scanf_s("%d", &n);
+	printf("0 ");
+	while (t1 < n)
 	{
 		t3 = t1 + t2;
 		t1 = t2;
 		t2 = t3;
-		count++;
-	};
-	printf("%d", t3);
+		if (t1 % 2 == 0)
+		{
+			keep += t1;
+			printf("+ %d ", t1);
+		}
+	}
+	printf("= %d \n\nsum of all even term : %d", keep, keep);
 	return 0;
 }
