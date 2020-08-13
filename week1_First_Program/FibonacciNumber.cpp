40a9d3b3e5ef5e0574f9a57e4fb0d953bfba73aa
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-08-13 20:38:49 +0700

    add user interface

--- week1_First_Program/FibonacciNumber.cpp
+++ week1_First_Program/FibonacciNumber.cpp
@@ -4,26 +4,21 @@ int main()
 	int n,t1,t2,t3=0,count=0,keep=0,i ;
 	t1 = 0;
 	t2 = 1;
+	printf("Program sum of all even term in fibonacci sequence");
+	printf("\n\nEnter number of term : ");
 	scanf_s("%d",&n);
-	//printf("%d, %d ", t1, t2);
-	//for(i=0;i<n;i++)
+	printf("0 ");
 	while(t1<n)
 	{
-		//printf("\nt[%d] : %d ", i, t1);
 		t3 = t1 + t2;
 		t1 = t2;
 		t2 = t3;
-		//i++;
 		if (t1 % 2 == 0)
 		{
-			printf("\nkeep = keep + t1");
-			printf("\n%d = %d + %d", keep + t1, keep, t1);
 			keep += t1;
-
+			printf("+ %d ", t1);
 		}
 	}
-	
-	printf("\nsum of all even term : %d", keep);
-
+	printf("= %d \n\nsum of all even term : %d",keep, keep);
 	return 0;
 }
